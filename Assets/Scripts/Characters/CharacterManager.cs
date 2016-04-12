c3844afda6774ada9a4124c8bf5c9fa874dca12a
zephyr <f.allard@student.isartdigital.com>
2016-04-12 17:15:00 +0200

    assets sound & sprites folders

--- Assets/Scripts/Characters/CharacterManager.cs
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Random;
 
 public class CharacterManager : MonoBehaviour {
 
@@ -12,6 +14,39 @@ public class CharacterManager : MonoBehaviour {
 		Seductive,
 		Bipolar
 	}
+
+	public enum Race
+	{
+		Human,
+		Vampire,
+		Animal
+	}
+
+	public List<Sprite> eyesPool;
+	public List<Sprite> mouthesPool;
+	public List<Sprite> hairCutPool;
+	public List<Sprite> dressPool;
+	public List<Sprite> accessoryPool;
+	public List<string> namePool;
+
+	public List<Sprite> moodPool;
+
+	// public List<Character> charaList;
+
+	// public Character customCharacter;
+
+	public void CreateRandomChar()
+	{
+		// customCharacter = new Character();
+		Sprite customCharacterEyes = eyesPool[Random.Range(0, eyesPool.Count-1)];
+		Sprite customCharacterMouth = mouthesPool[Random.Range(0, mouthesPool.Count-1)];
+		Sprite customCharacterHairCut = hairCutPool[Random.Range(0, hairCutPool.Count-1)];
+		Sprite customCharacterDress = dressPool[Random.Range(0, dressPool.Count-1)];
+
+
+	}
+
+	public void GetAssociatedSet(){}
 	// Use this for initialization
 	void Start () {
 	
