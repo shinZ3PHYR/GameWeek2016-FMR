dca6d41bf0e21d2d9b57004d852ad7ad0c1c0cbf
zephyr <f.allard@student.isartdigital.com>
2016-04-12 16:36:42 +0200

    Char Manager

---
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour {
+
+	public enum Type
+	{
+		Shy,
+		Haughty,
+		Tomboy,
+		HungUp,
+		Seductive,
+		Bipolar
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
