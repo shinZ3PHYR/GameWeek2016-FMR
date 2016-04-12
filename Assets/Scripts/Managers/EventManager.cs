1d411fc89e8a80e3f01ce97c93fbf01d2ee6679b
Zobuko <l.boulanger@student.isartdigital.com>
2016-04-12 15:56:10 +0200

    first push :p

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void MenuAction();
+    public static event MenuAction OnMenu;
+
+    public delegate void UIAction(float vertAxis);
+    public static event UIAction OnUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            OnMenu();
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            OnUI(Input.GetAxis("Vertical"));
+        }
+	}
+}
---
+++ Assets/Scripts/Managers/HudManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour {
+
+    public Image menu;
+    public Image hud;
+
+    private Vector3 menuPosition;
+    private Vector3 hudPosition;
+
+	// Use this for initialization
+	void Start () {
+        EventManager.OnMenu += ShowMenu;
+        EventManager.OnUI += ShowHud;
+
+        menuPosition = menu.transform.position;
+        menuPosition.y -= Screen.height;
+        menu.transform.position = menuPosition;
+
+        hudPosition = hud.transform.position;
+        hudPosition.y -= Screen.height;
+        hud.transform.position = hudPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+    void ShowMenu()
+    {
+        menuPosition = menu.transform.position;
+        menuPosition.y += Screen.height;
+        menu.transform.position = menuPosition;
+        EventManager.OnMenu -= ShowMenu;
+    }
+
+    void ShowHud(float vertAxis)
+    {
+        hudPosition = hud.transform.position;
+        if (vertAxis > 0)
+            hudPosition.y += Screen.height;
+        else
+            hudPosition.y -= Screen.height;
+        hud.transform.position = hudPosition;
+        //EventManager.OnMenu -= ShowMenu;
+    }
+}
