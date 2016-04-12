ece19a195e8c9baa815312da2ccfff7e8b1d06e6
zephyr <f.allard@student.isartdigital.com>
2016-04-12 14:44:13 +0200

    DialogueBoxManager

---
+++ Assets/Scripts/Effects/TextAnimation.cs
@@ -0,0 +1,28 @@
+﻿// using UnityEngine;
+// using System.Collections;
+// using UnityEngine.UI;
+
+// public class TextAnimation : MonoBehaviour {
+
+// 	// Use this for initialization
+
+// 	[RequireComponent(Text)]
+// 	public Text textComponent;
+
+// 	IEnumerator fadeLetter(String dialogueString)
+// 	{
+// 		for(var i = 0; i<dialogueString.Length; i++)
+// 		{
+// 			dialogueString[i].color.a = 1;
+// 		}
+// 	}
+
+// 	void Start () {
+	
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+	
+// 	}
+// }
