41de45a79623fb073dedbb23dadc76cf08efb515
zephyr <f.allard@student.isartdigital.com>
2016-04-14 11:54:24 +0200

    Particles + linkage du GameManager au CharManager

--- Assets/Scripts/Managers/HudManager.cs
+++ Assets/Scripts/Managers/HudManager.cs
@@ -1,70 +1,70 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿// using UnityEngine;
+// using System.Collections;
+// using UnityEngine.UI;
 
-public class HudManager : MonoBehaviour {
+// public class HudManager : MonoBehaviour {
 
-    public Transform menu;
-    public Image hud;
+//     public Transform menu;
+//     public Image hud;
 
-    private Vector3 hudPosition;
+//     private Vector3 hudPosition;
 
-	// Use this for initialization
-	void Start () {
-        EventManager.OnMenu += ShowMenu;
-        EventManager.OnUI += ShowHud;
+// 	// Use this for initialization
+// 	void Start () {
+//         EventManager.OnMenu += ShowMenu;
+//         EventManager.OnUI += ShowHud;
 
-        menu.gameObject.SetActive(false);
+//         menu.gameObject.SetActive(false);
 
-        hudPosition = hud.transform.position;
-        hudPosition.y -= Screen.height;
-        hud.transform.position = hudPosition;
-	}
+//         hudPosition = hud.transform.position;
+//         hudPosition.y -= Screen.height;
+//         hud.transform.position = hudPosition;
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
+// 	// Update is called once per frame
+// 	void Update () {
 	    
-	}
+// 	}
 
 
-    void ShowMenu()
-    {
-        menu.gameObject.SetActive(true);
-        EventManager.OnMenu -= ShowMenu;
-    }
+//     void ShowMenu()
+//     {
+//         menu.gameObject.SetActive(true);
+//         EventManager.OnMenu -= ShowMenu;
+//     }
 
-    void ShowHud(float vertAxis)
-    {
-        hudPosition = hud.transform.position;
-        if (vertAxis > 0)
-        {
-            hudPosition.y += Screen.height;
-        }
-        else
-        {
-            hudPosition.y -= Screen.height;
-        }
-        hud.transform.position = hudPosition;
-    }
+//     void ShowHud(float vertAxis)
+//     {
+//         hudPosition = hud.transform.position;
+//         if (vertAxis > 0)
+//         {
+//             hudPosition.y += Screen.height;
+//         }
+//         else
+//         {
+//             hudPosition.y -= Screen.height;
+//         }
+//         hud.transform.position = hudPosition;
+//     }
     
-    public void ResumePress()
-    {
-        menu.gameObject.SetActive(false);
-        EventManager.OnMenu += ShowMenu;
-    }
+//     public void ResumePress()
+//     {
+//         menu.gameObject.SetActive(false);
+//         EventManager.OnMenu += ShowMenu;
+//     }
 
-    public void RestartPress()
-    {
-        // reload lvl
-    }
+//     public void RestartPress()
+//     {
+//         // reload lvl
+//     }
 
-    public void MainMenuPress()
-    {
-        Application.LoadLevel(0);
-    }
+//     public void MainMenuPress()
+//     {
+//         Application.LoadLevel(0);
+//     }
 
-    public void ExitPress()
-    {
-        Application.Quit();
-    }
-}
+//     public void ExitPress()
+//     {
+//         Application.Quit();
+//     }
+// }
